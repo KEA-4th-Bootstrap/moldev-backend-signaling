4f5f86b4c3ea324efcc26b5c50336a7e3eded7de
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-29 20:51:00 +0900

    KBB-52 [feat] join room 로직 구현

--- src/server.js
+++ src/server.js
@@ -12,8 +12,21 @@ app.get("/*", (_, res) => res.redirect("/"))
 const httpServer = http.createServer(app);
 const io = new Server(httpServer);
 
+let receiverPCs = {};
+let senderPCs = {};
+let users = {};
+let socketToRoom = {};
+
 io.on("connection", (socket) => {
-  console.log(socket)
+  socket.on("joinRoom", data => {
+    try {
+        let allUsers = getOtherUsersInRoom(data.id, data.roomID);
+        io.to(data.id).emit("allUsers", { users: allUsers });
+    } catch (error) {
+        console.log(error);
+    }
+  });
+
 })
 
 const handleListen = () => console.log('Listen one http://localhost:3000,')
