528e00df39201818cb1d29ed38adfba4568ee28e
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-07 00:00:44 +0900

    KBB-54 [feat] turn server 추가

--- src/config.js
+++ src/config.js
@@ -5,14 +5,15 @@ module.exports = {
   SERVER_PORT: process.env.SERVER_PORT,
   PC_CONFIG: {
     iceServers: [
-    // {
-    //   urls: 'stun:[STUN_IP]:[PORT]',
-    //   'credentials': '[YOR CREDENTIALS]',
-    //   'username': '[USERNAME]'
-    // },
       {
           urls: process.env.STUN_URL,
-      }]
+      },
+      {
+        urls: process.env.TURN_URL,
+        username: process.env.TURN_USERNAME,
+        credential: process.env.TURN_CREDETIAL,
+      },
+    ]
   },
   SOCKET_SERVER_URL: process.env.SOCKET_SERVER_URL,
   CORS_CONFIG: {
@@ -23,5 +24,8 @@ module.exports = {
         credentials: true                 
     },
     allowEIO3: true
+  },
+  REDIS_URL: {
+    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
   }
 };
