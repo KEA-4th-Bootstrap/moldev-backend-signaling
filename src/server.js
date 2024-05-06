e0a29bb8370816f6db1ecd4f45691b00eb6d4c4f
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-07 00:06:47 +0900

    KBB-54 [feat] 불필요한 로그 제거 및 room에 해당하는 socket publish 등록

--- src/server.js
+++ src/server.js
@@ -3,9 +3,7 @@ import { Server } from "socket.io";
 import express from 'express'
 import cors from 'cors'
 import config from './config'
-import PeerConnectionManager from './peer'
-import RoomManager from './room'
-let wrtc = require("wrtc");
+import { Subscribe, Publish, Redis } from './redis'
 
 const app = express();
 app.use(cors());
@@ -14,89 +12,54 @@ app.use("/public", express.static(__dirname + "/public"));
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, config.CORS_CONFIG);
 
-const room = new RoomManager();
-const peer = new PeerConnectionManager(room);
+const pub = new Publish();
+const redis = new Redis();
 
-io.on("connection", (socket) => {
-  socket.on("joinRoom", (data) => {
-    try {
-      let allUsers = room.getOtherUsersInRoom(data.id, data.roomID);
-      io.to(data.id).emit("allUsers", { users: allUsers });
-    } catch (error) {
-      console.log(error);
-    }
+io.on("connection", async (socket) => {
+  const sub = new Subscribe(io, socket);
+  
+  socket.on("joinRoom", async (data) => {
+    const hostId = await redis.getAsync(`room:${data.roomId}`);
+    console.log("joinRoom", data.roomId);
+    await pub.publish((hostId === null ? socket.id : hostId) + "-joinRoom", JSON.stringify(data));
   });
 
   socket.on("senderOffer", async (data) => {
-    try {
-      room.socketToRoom[data.senderSocketID] = data.roomID;
-      let pc = peer.createReceiverPeerConnection(data.senderSocketID, socket, data.roomID);
-      await pc.setRemoteDescription(data.sdp);
-      let sdp = await pc.createAnswer({
-        offerToReceiveAudio: true,
-        offerToReceiveVideo: true,
-      });
-      await pc.setLocalDescription(sdp);
-      socket.join(data.roomID);
-      io.to(data.senderSocketID).emit("getSenderAnswer", { sdp });
-    } catch (error) {
-      console.error(error);
-    }
+    const hostId = await redis.getAsync(`room:${data.roomId}`);
+    console.log("senderOffer", data.roomId);
+    pub.publish((hostId === null ? socket.id : hostId) + "-senderOffer", JSON.stringify(data));
   });
 
   socket.on("senderCandidate", async (data) => {
-    try {
-      let pc = peer.receiverPCs[data.senderSocketID];
-      // console.log(pc, data.senderSocketID)
-      await pc.addIceCandidate(new wrtc.RTCIceCandidate(data.candidate));
-    } catch (error) {
-      console.error(error);
-    }
+    const hostId = await redis.getAsync(`room:${data.roomId}`);
+    console.log("senderCandidate", data.roomId);
+    pub.publish((hostId === null ? socket.id : hostId) + "-senderCandidate", JSON.stringify(data));
   });
 
   socket.on("receiverOffer", async (data) => {
-    try {
-      let pc = peer.createSenderPeerConnection(data.receiverSocketID, data.senderSocketID, socket, data.roomID);
-      await pc.setRemoteDescription(data.sdp);
-      let sdp = await pc.createAnswer({
-        offerToReceiveAudio: false,
-        offerToReceiveVideo: false,
-      });
-      await pc.setLocalDescription(sdp);
-      io.to(data.receiverSocketID).emit("getReceiverAnswer", {
-        id: data.senderSocketID,
-        sdp,
-      });
-    } catch (error) {
-      console.error(error);
-    }
+    const hostId = await redis.getAsync(`room:${data.roomId}`);
+    console.log("receiverOffer", data.roomId);
+    pub.publish((hostId === null ? socket.id : hostId) + "-receiverOffer", JSON.stringify(data));
   });
 
   socket.on("receiverCandidate", async (data) => {
-    try {
-      const senderPC = peer.senderPCs[data.senderSocketID].filter((sPC) => sPC.id === data.receiverSocketID)[0];
-      await senderPC.pc.addIceCandidate(
-        new wrtc.RTCIceCandidate(data.candidate)
-      );
-    } catch (error) {
-      console.error(error);
-    }
+    const hostId = await redis.getAsync(`room:${data.roomId}`);
+    console.log("receiverCandidate", data.roomId);
+    pub.publish((hostId === null ? socket.id : hostId) + "-receiverCandidate", JSON.stringify(data));
   });
 
-  socket.on("disconnect", () => {
-    try {
-      let roomID = room.socketToRoom[socket.id];
-
-      room.deleteUser(socket.id, roomID);
-      peer.closeReceiverPC(socket.id);
-      peer.closeSenderPCs(socket.id);
-
-      socket.broadcast.to(roomID).emit("userExit", { id: socket.id });
-    } catch (error) {
-      console.error(error);
-    }
+  socket.on("disconnect", async () => {
+    pub.publish(socket.id + "-disconnect", JSON.stringify({}));
   });
+
+  await sub.subscribeSendInfoUsingIo(socket.id);
+  await sub.subscribeJoinRoom(socket.id);
+  await sub.subscribeSenderOffer(socket.id, socket);
+  await sub.subscribeSenderCandidate(socket.id);
+  await sub.subscribeReceiverOffer(socket.id, socket);
+  await sub.subscribeReceiverCandidate(socket.id);
+  await sub.subscribeDisconnect(socket.id, socket);
 });
 
-const handleListen = () => console.log('Listen one ' + config.SOCKET_SERVER_URL)
+const handleListen = () => console.log('Listen one' + config.SOCKET_SERVER_URL)
 httpServer.listen(config.SERVER_PORT, handleListen);
